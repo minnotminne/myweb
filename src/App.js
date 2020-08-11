32f08c05c74ccc900c00880c3f7953f92bd10451
minnotminne <minnotminne2@naver.com>
2020-08-11 21:14:01 +0900

    DAY 1. Final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
     <div>
-    <p> 안녕하 세요 ! </p>
+    <p> 안녕하 세 요 ! </p>
     <Button Primary> hi </Button>
     <Progress percent = {33} indicating/>
     </div>
